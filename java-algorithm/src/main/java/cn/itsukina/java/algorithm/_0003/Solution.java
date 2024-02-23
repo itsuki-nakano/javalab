1ceed6726048839d9874de9735a736f9a5c1761c
Jiajun Chen* <1014340935@qq.com>
2024-02-23 14:45:20 +0800

    feat: 添加算法题解

---
+++ java-algorithm/src/main/java/cn/itsukina/java/algorithm/_0003/Solution.java
@@ -0,0 +1,27 @@
+package cn.itsukina.java.algorithm._0003;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int left = -1, right = 0;
+        int length = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            left = Math.max(left, map.getOrDefault(c, -1));
+            map.put(c, right);
+            length = Math.max(length, right - left);
+            right++;
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        String s = "pwwkew";
+        Solution solution = new Solution();
+        int length = solution.lengthOfLongestSubstring(s);
+        System.out.println(length);
+    }
+}
