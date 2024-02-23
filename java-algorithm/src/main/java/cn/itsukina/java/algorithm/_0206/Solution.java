4a6a48cedc50af9bf448e5dedfd7c2ca4ef9810f
Jiajun Chen* <1014340935@qq.com>
2024-02-23 17:41:12 +0800

    feat: 添加算法题解 0206

---
+++ java-algorithm/src/main/java/cn/itsukina/java/algorithm/_0206/Solution.java
@@ -0,0 +1,33 @@
+package cn.itsukina.java.algorithm._0206;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    // 递归
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = null;
+        ListNode newHead = reverseList(next);
+        next.next = head;
+        return newHead;
+    }
+}
