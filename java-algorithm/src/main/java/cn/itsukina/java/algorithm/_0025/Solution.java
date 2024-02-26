742d63b9f0778ba8faccd66996d58ecf7eef3dde
Jiajun Chen <1014340935@qq.com>
2024-02-26 14:59:48 +0800

    feat: 添加算法题解 0025 0215

---
+++ java-algorithm/src/main/java/cn/itsukina/java/algorithm/_0025/Solution.java
@@ -0,0 +1,50 @@
+package cn.itsukina.java.algorithm._0025;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) {
+            return null;
+        }
+        ListNode b = head;
+        for (int i = 0; i < k - 1; i++) {
+            // 不足 K 个不反转
+            if (b.next == null) {
+                return head;
+            }
+            b = b.next;
+        }
+        ListNode next = b.next;
+        b.next = null;
+        ListNode newHead = reverse(head);
+        head.next = reverseKGroup(next, k);
+        return newHead;
+    }
+
+    private ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = null;
+        ListNode newHead = reverse(next);
+        next.next = head;
+        return newHead;
+    }
+}
---
+++ java-algorithm/src/main/java/cn/itsukina/java/algorithm/_0215/Solution.java
@@ -0,0 +1,43 @@
+package cn.itsukina.java.algorithm._0215;
+
+public class Solution {
+
+    public int findKthLargest(int[] nums, int k) {
+        int i = 0, j = nums.length - 1;
+        quickSort(nums, k - 1, i, j);
+        return nums[k - 1];
+    }
+
+    private void quickSort(int[] nums, int k, int i, int j) {
+
+        if (i >= j) {
+            return;
+        }
+        int m = partition(nums, k, i, j);
+        if (m == k) {
+            return;
+        }
+        quickSort(nums, k, i, m - 1);
+        quickSort(nums, k, m + 1, j);
+    }
+
+    private int partition(int[] nums, int k, int low, int high) {
+        int i = low, j = high + 1;
+        while (true) {
+            while (i != high && nums[++i] > nums[low]) ;
+            while (j != low && nums[--j] < nums[low]) ;
+            if (i >= j) {
+                break;
+            }
+            swap(nums, i, j);
+        }
+        swap(nums, j, low);
+        return j;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
